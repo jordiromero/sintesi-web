d2b198f9a10d9664b51e1cc4a7cff8f279467f2a
jordiromero <jordiromero@iesebre.com>
2014-05-19 11:22:02 +0200

    pàgina principal

--- application/views/inici.php
+++ application/views/inici.php
@@ -1,7 +1,28 @@
 <body>
+	<style type="text/css">
+	table {
+		width: 100%;
+		border: 2px;
+		text-align: center;
+
+	}
+
+
+	</style>
 <div class='container'>
 	
 <h1>P&agrave;gina inicial Cr&egrave;dit de Sintesi</h1>
+	<table class="principal">
+		<tr>
+			<td><img src="<?php echo base_url();?>assets/images/principal/menu_crear.jpg"><br>Crea Menú</td>
+			<td><img src="<?php echo base_url();?>assets/images/principal/menu_consultar.jpg"><br>Consulta Carta</td>
+			<td>Crea Reserva</td>
+		</tr>
+		<tr>
+			<td><img src="<?php echo base_url();?>assets/images/principal/buscar_reserva.jpg"><br>Consulta Reserva</td>
+			<td><img src="<?php echo base_url();?>assets/images/principal/user_add.jpg"><br>Usuaris</td>
+		</tr>
+	</table>
 </div>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
